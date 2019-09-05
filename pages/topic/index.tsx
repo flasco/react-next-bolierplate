931ded2933d243ede825114db57fb071dca6ca8e
lear <1325925707@qq.com>
2019-09-06 00:36:01 +0800

    fix: 添加简单的网络请求演示

--- pages/topic/index.tsx
+++ pages/topic/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { NextPage } from 'next';
 
 import styles from './index.scss';
 
-const Topic = () => {
+const Topic: NextPage<{ data: any[] }> = ({ data }) => {
   return (
     <>
-    <span className={styles.abc}>123123</span>
-    <span className={'abc'}>abcabc</span>
+      <span className={styles.abc}>123123</span>
+      <span className={'abc'}>abcabc</span>
     </>
-  )
-}
+  );
+};
+
+// 需要 SSR 的网络请求放这里
+Topic.getInitialProps = async () => {
+  return { data: [] };
+};
 
 export default Topic;
