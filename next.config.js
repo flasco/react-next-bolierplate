c621c1538897111bf1fd0f1d600e06b4aaf7ee6e
lear <1325925707@qq.com>
2019-09-05 23:38:03 +0800

    fix: add ui framework

--- next.config.js
+++ next.config.js
@@ -1,13 +1,52 @@
 // next.config.js
-const withSass = require('@zeit/next-sass');
-const withPlugins = require('next-compose-plugins/lib'); //结合sass css
+const withStyles = require('./thrid-party/styles');
+const withPlugins = require('next-compose-plugins/lib');
+const lessToJS = require('less-vars-to-js');
+const fs = require('fs');
+const path = require('path');
+
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+);
+
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = file => {};
+  require.extensions['.scss'] = file => {};
+}
 
 module.exports = withPlugins(
   [
-    withSass({
+    withStyles({
       cssModules: true,
       cssLoaderOptions: {
         localIdentName: '[local]___[hash:base64:5]',
+      },
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+        modifyVars: themeVariables, // make your antd custom effective
+      },
+      webpack: (config, { isServer }) => {
+        if (isServer) {
+          const antStyles = /antd\/.*?\/style.*?/;
+          const origExternals = [...config.externals];
+          config.externals = [
+            (context, request, callback) => {
+              if (request.match(antStyles)) return callback();
+              if (typeof origExternals[0] === 'function') {
+                origExternals[0](context, request, callback);
+              } else {
+                callback();
+              }
+            },
+            ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+          ];
+
+          config.module.rules.unshift({
+            test: antStyles,
+            use: 'null-loader',
+          });
+        }
+        return config;
       },
     }),
   ],
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'antd';
+
+
+const Home = () => (
+  <div>
+    Hellp
+    <Button type="primary">测试</Button>
+  </div>
+)
+
+export default Home
