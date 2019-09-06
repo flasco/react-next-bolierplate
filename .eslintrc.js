2fad994a3ac08391bd74ac39d6a4bc67a04a7975
czq <chenzhengqiang@youzan.com>
2019-09-06 11:49:38 +0800

    fix: add lint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  rules: {
+    quotes: ['error', 'single'],
+    eqeqeq: [
+      'error',
+      'always',
+      {
+        null: 'ignore',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      parser: 'babel-eslint',
+      extends: ['plugin:prettier/recommended', 'plugin:react/recommended'],
+      plugins: ['react'],
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:prettier/recommended',
+        'prettier/react',
+        'prettier/@typescript-eslint',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      plugins: ['react', '@typescript-eslint'],
+
+      rules: {
+        // @fixable 必须使用 === 或 !==，禁止使用 == 或 !=，与 null 比较时除外
+        '@typescript-eslint/indent': ['error', 2],
+        // 类和接口的命名必须遵守帕斯卡命名法，比如 PersianCat
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/no-unused-vars': 2,
+        '@typescript-eslint/no-explicit-any': 0,
+      },
+    },
+  ],
+};
